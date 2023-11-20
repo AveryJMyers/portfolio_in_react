cf3dd5d77c7d4429836527b3bbcb6088b37e2251
Avery Myers <averyjmyers@hotmail.com>
2023-11-19 16:43:51 -0800

    updated react routing

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import StyledButton from '../Components/StyledButton';
 import LandingNav from '../Components/LandingNav';
+
+import phonePic from '../Photos/phone4.png';
+import emailPic from '../Photos/email.png';
+import linkedInPic from '../Photos/linkedInLM.png';
+import gitPic from '../Photos/git.png';
+
 export default function ContactList() {
     return (
     <>
         <section>
             <div className='flex justify-center rounded  mt-4 p-4 items-center '>
                 <a href="" className='w-10 pr-3 mr-2 scaleHover'>
-                    <img src="../Photos/phone4.png" className="  scaleHover" />
+                    <img src={phonePic} className="  scaleHover" />
                 </a>
                 <a href="https://github.com/AveryJMyers" className= "w-10 pr-3 mr-2 scaleHover">
-                    <img src="../Photos/git.png" className="" />
+                    <img src={gitPic} className="" />
                 </a>
                 <a href="mailto:averyjmyers@outlook.com" className='w-10 pr-3 mr-2 scaleHover'>
-                    <img src="../Photos/email.png" className="" />
+                    <img src={emailPic} className="" />
                 </a>
                 <a href="https://www.linkedin.com/in/avery-m-108704110/" className=' w-10 pr-3 mr-2  scaleHover'>
-                    <img src="../Photos/linkedInLM.png" className=" " />
+                    <img src={linkedInPic} className=" " />
                 </a>
             </div>
         </section>
