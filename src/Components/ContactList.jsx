b641ff84a379b424b9fa0a1630a29bc044377088
Avery Myers <averyjmyers@hotmail.com>
2023-11-19 16:24:57 -0800

    more photo fixes

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -7,16 +7,16 @@ export default function ContactList() {
         <section>
             <div className='flex justify-center rounded  mt-4 p-4 items-center '>
                 <a href="" className='w-10 pr-3 mr-2 scaleHover'>
-                    <img src="../public/photos/phone4.png" className="  scaleHover" />
+                    <img src="../photos/phone4.png" className="  scaleHover" />
                 </a>
                 <a href="https://github.com/AveryJMyers" className= "w-10 pr-3 mr-2 scaleHover">
-                    <img src="../public/photos/git.png" className="  " />
+                    <img src="../photos/git.png" className="" />
                 </a>
                 <a href="mailto:averyjmyers@outlook.com" className='w-10 pr-3 mr-2 scaleHover'>
-                    <img src="../public/photos/email.png" className="" />
+                    <img src="../photos/email.png" className="" />
                 </a>
                 <a href="https://www.linkedin.com/in/avery-m-108704110/" className=' w-10 pr-3 mr-2  scaleHover'>
-                    <img src="../public/photos/linkedInLM.png" className=" " />
+                    <img src="../photos/linkedInLM.png" className=" " />
                 </a>
             </div>
         </section>
